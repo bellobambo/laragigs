1f41bbf02e0f2f475bfe2487590a74fbac2c20be
bellobambo <bellobambo21@gmail.com>
2024-01-02 11:10:58 +0100

    edit

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -35,21 +35,36 @@ class ListingController extends Controller
 
     public function store(Request $request)
     {
-        // dd($request->all());
+        // dd($request->file('logo'));
         $formfields = $request->validate([
             'title' => 'required',
-            'company' => ['required ' , Rule::unique('listings' , 'company')],
-            'location' => 'required' ,
+            'company' => ['required ', Rule::unique('listings', 'company')],
+            'location' => 'required',
             'website' => 'required',
-            'email' => ['required' , 'email'],
-            'tags' => 'required' ,
+            'email' => ['required', 'email'],
+            'tags' => 'required',
             'description' => 'required'
         ]);
+
+        if ($request->hasFile('logo')) {
+            $formfields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
 
         Listing::create($formfields);
 
 
-        return redirect('/')->with('message' , 'Listing Created Successfully!');
+        return redirect('/')->with('message', 'Listing Created Successfully!');
     }
+
+    //Show Edit Form
+
+    public function edit(Listing $listing)
+    {
+        dd($listing->title);
+        return view('listings.edit', ['listing' => $listing]);
+    }
+
+
 
 }
