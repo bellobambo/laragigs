14693b4a9274149fa949cd110af4d62b33a6b5b4
bellobambo <bellobambo21@gmail.com>
2024-01-03 00:12:30 +0100

    login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -13,7 +15,50 @@ class UserController extends Controller
     {
 
         return view('users.register');
-        
+
+    }
+
+
+    // Create  New Users
+
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'name' => ['required', 'min:3'],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        // Hash Password
+        $formFields['password'] = bcrypt($formFields['password']);
+
+
+        // Create User
+        $user = User::create($formFields);
+
+        // Login
+
+        auth()->login($user);
+
+        return redirect('/')->with('message', 'User Created and Logged');
+
+
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+
+        return redirect('/')->with('message' , 'you have been logged gout! ');
+    }
+
+
+    public function login(){
+        return view('users.login');
     }
 
 }
